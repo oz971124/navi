ffcdb1e5f64f013c1453f9bb4da302b059376f5c
Juhwan Jung <oz971124@gmail.com>
2023-12-09 19:23:20 +0900

    개발환경 세팅

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() :
+    return render_template('index.html')
+
+@app.route('/page')
+def page() :
+    return render_template('page.html')
+
+
+if __name__ == "__main__" :
+    app.run(debug = True)
